a914941e699523a539065cfe4d97b1496253d86c
sklianchukk <sklianchuk111@gmail.com>
2023-11-21 08:35:51 +0200

    I add lab5 and write task in README file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # lab5
+## Варіант 26
+Додати клас Country (name, capital, code,  population, area, GDP). Додати enum governmentType: DEMOCRACY, REPUBLIC, AUTOCRACY, … Додати клас Land (name, ...), до якого можуть належати певні країни. Додати функцію підрахунку  густини населення серед певних країн сalculatePopulationDensity (Countries*). Реалізувати метод сортування  за GDP та виводу топу країн з найкращою економікою. Реалізувати метод для вибору країни, котрий залежить від 
---
+++ lab5.py
@@ -0,0 +1,84 @@
+from enum import Enum
+
+class GovernmentType(Enum):
+    DEMOCRACY = "Democracy"
+    REPUBLIC = "Republic"
+    AUTOCRACY = "Autocracy"
+    MIXED_SYSTEM = "Mixed_system"
+
+
+class Country:
+
+    name = None
+    capital = None
+    code = None
+    population = None
+    area = None
+    GPD = None
+    govern = None #type of Goverment
+
+    def __init__(self, name, capital, code, population, area, GPD, govern):
+        #population in a million, area in km^2, gpd in a billion
+        self.name = name
+        self.capital = capital
+        self.code = code
+        self.population = population
+        self.area = area
+        self.GPD = GPD
+        self.govern = govern
+
+
+class Land:
+    def __init__(self, name):
+        self.name = name
+        self.countries = []
+
+    def add_country(self, country):
+        self.countries.append(country)
+
+
+def density(countries):
+    for country in countries:
+        density = country.population * 10 ** 6 / country.area # multiply by 10^6, cause country.population I saved in million
+        print(f"{country.name}: Density - {round(density, 2)} people per square km")
+    print()
+
+def sort_countries_by_gdp(countries):
+    sorted_countries = sorted(countries, key=lambda country: country.GPD, reverse=True)
+    return sorted_countries
+
+
+def selected_countries(countries, criteria):
+    selected_countries = [country for country in countries if criteria(country)]
+    return selected_countries
+
+country1 = Country("Ukraine", "Kyiv", "+380",33.2,603628, 173.413, "Mixed_system")
+country2 = Country("USA", "Washington", "+1",333.287, 9833520,  26950, "Republic")
+country3 = Country("Qatar", "Doha", "+974",2.795, 11581, 328.134, "Autocracy")
+country4 = Country("Honduras", "Tegucigalpa", "+504",9.571, 112492, 33.992, "Republic")
+
+land1 = Land("Asia")
+land2 = Land("Europe")
+land3 = Land("Africa")
+land4 = Land("America")
+
+land1.add_country(country4)
+land2.add_country(country1)
+land3.add_country(country3)
+land4.add_country(country2)
+
+countries_for_density = [country1, country2, country3, country4]
+
+density(countries_for_density)
+
+sorted_countries = sort_countries_by_gdp(countries_for_density)
+
+for i, country in enumerate(sorted_countries, 1):
+    print(f"{i}. Country: {country.name}, GDP: {country.GPD}")
+print()
+
+selected_criteria = lambda country: len(country.name) > 5
+selected_countries = selected_countries(countries_for_density, selected_criteria)
+
+for i, country in enumerate(selected_countries, 1):
+    print(f"{i}. Selected country: {country.name}")
