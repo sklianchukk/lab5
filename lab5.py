ddf46999e24a29219ad6eb987dc320a9cb0e4531
sklianchukk <151012687+sklianchukk@users.noreply.github.com>
2024-01-14 12:09:34 +0100

    commit-for-zveres-ss-lab5.py
    In this commit I deleted some useless strings and incorporated part of code into Main method

--- lab5.py
+++ lab5.py
@@ -8,15 +8,6 @@ class GovernmentType(Enum):
 
 
 class Country:
-
-    name = None
-    capital = None
-    code = None
-    population = None
-    area = None
-    gpd = None
-    govern = None #type of Goverment
-
     def __init__(self, name, capital, code, population, area, gpd, govern):
         #population in a million, area in km^2, gpd in a billion
         self.name = name
@@ -52,32 +43,40 @@ def selected_countries(countries, criteria):
     selected_countries = [country for country in countries if criteria(country)]
     return selected_countries
 
-country1 = Country("Ukraine", "Kyiv", "+380",33.2,603628, 173.413, "Mixed_system")
-country2 = Country("USA", "Washington", "+1",333.287, 9833520,  26950, "Republic")
-country3 = Country("Qatar", "Doha", "+974",2.795, 11581, 328.134, "Autocracy")
-country4 = Country("Honduras", "Tegucigalpa", "+504",9.571, 112492, 33.992, "Republic")
+def main():
+    """
+    Creating Main function for storing data
+    """
+    country1 = Country("Ukraine", "Kyiv", "+380", 33.2, 603628, 173.413, "Mixed_system")
+    country2 = Country("USA", "Washington", "+1", 333.287, 9833520, 26950, "Republic")
+    country3 = Country("Qatar", "Doha", "+974", 2.795, 11581, 328.134, "Autocracy")
+    country4 = Country("Honduras", "Tegucigalpa", "+504", 9.571, 112492, 33.992, "Republic")
 
-land1 = Land("Asia")
-land2 = Land("Europe")
-land3 = Land("Africa")
-land4 = Land("America")
+    land1 = Land("Asia")
+    land2 = Land("Europe")
+    land3 = Land("Africa")
+    land4 = Land("America")
 
-lands = [land1, land2, land3, land4]
-countries = [country4, country1, country3, country2]
+    lands = [land1, land2, land3, land4]
+    countries = [country4, country1, country3, country2]
 
-for i in range(len(lands)):
-    lands[i].add_country(countries[i])
+    for i in range(len(lands)):
+        lands[i].add_country(countries[i])
 
-density(countries)
+    density(countries)
 
-sorted_countries = sort_countries_by_gdp(countries)
+    sorted_countries = sort_countries_by_gpd(countries)
 
-for i, country in enumerate(sorted_countries, 1):
-    print(f"{i}. Country: {country.name}, GDP: {country.gpd}")
-print()
+    for i, country in enumerate(sorted_countries, 1):
+        print(f"{i}. Country: {country.name}, GPD: {country.gpd}")
+    print()
 
-selected_criteria = lambda country: len(country.name) > 5
-selected_countries = selected_countries(countries, selected_criteria)
+    selected_criteria = lambda country: len(country.name) > 5
+    selected_countries_result = selected_countries(countries, selected_criteria)
 
-for i, country in enumerate(selected_countries, 1):
-    print(f"{i}. Selected country: {country.name}")
+    for i, country in enumerate(selected_countries_result, 1):
+        print(f"{i}. Selected country: {country.name}")
+
+
+if __name__ == "__main__":
+    main()
